b867bec47281cd352fe3a19e5ee0255e848ea101
DC-Melo <melodachor@gmail.com>
2024-03-21 15:58:51 +0800

    not import test

--- spring-boot-hello-world/src/test/java/com/dc/HelloControllerTests2.java
+++ spring-boot-hello-world/src/test/java/com/dc/HelloControllerTests2.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 /**
  * HelloControllerTest2
  */
